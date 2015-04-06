167491a02a438ddb6c334f3c6a33e808a499d673
Ivan Udovichenko <tlzr@mymailbox.pp.ua>
2015-04-06 10:55:11 +0300

    Make some changes

--- make_sequence.py
+++ make_sequence.py
@@ -11,54 +11,60 @@ class MakeTree:
         self.workbook = []
         self.role_workbook = []
         self.dependencies = []
-        self.processed_dependencies = []
+        self.id_n = 0
 
     def get_role(self, role):
-        for role in self.role_workbook:
-            if role in role['groups']:
-                return role
+        for group in self.role_workbook:
+            type = group.get('type', None)
+            if role in group['groups'] and type == 'puppet':
+                return group
         return None
 
     def store_dependencies(self, task):
-        if task is not None:
-            for dependencie in task:
-                if dependencie not in self.processed_dependencies:
+        requirements = task.get('requires', None)
+        if requirements is not None:
+            for dependencie in requirements:
+                if dependencie not in self.dependencies:
                     self.dependencies.append(dependencie)
 
     def get_task(self, dependencie):
         for task in self.workbook:
             id = task.get('id', None)
-            if id is not None and id == dependencie:
-                self._data[id] = {
-                    'puppet_modules': task.get('parameters', None).get('puppet_modules', None),
-                    'puppet_manifest': task.get('parameters', None).get('puppet_manifest', None),
-                }
-                self.store_dependencies(task.get('requires', None))
-                self.processed_dependencies.append(id)
+            parameters = task.get('parameters', None)
+            if id is not None and id == dependencie and parameters is not None:
+                self.fill_data(task)
+                self.store_dependencies(task)
 
     def get_data(self, role):
         role = self.get_role(role)
         if role is not None:
             id = role.get('id', None)
             if id is not None:
-                self._data[id] = {
-                    'puppet_modules': role.get('parameters', None).get('puppet_modules', None),
-                    'puppet_manifest': role.get('parameters', None).get('puppet_manifest', None),
-                }
-                self.store_dependencies(role.get('requires', None))
+                self.fill_data(role)
+                self.store_dependencies(role)
                 for dependencie in self.dependencies:
                     self.get_task(dependencie)
             else:
                 print 'ID is not found!'
         else:
             print 'Role has not been found. Aborting.'
+            sys.exit(1)
 
-        return self._data
+    def fill_data(self, data):
+        id = data.get('id', None)
+        self._data[self.id_n] = {
+            'id': id,
+            'puppet_modules': data.get('parameters', None).get('puppet_modules', None),
+            'puppet_manifest': data.get('parameters', None).get('puppet_manifest', None),
+        }
+        self.id_n += 1
 
     def build_sequence(self, role):
         self.get_data(role)
-        for id, puppet_properties in self.data.iteritems():
-            print puppet_properties.get('puppet_modules', None), puppet_properties.get('puppet_manifest', None),
+        self._data = sorted(self._data.items(), key=lambda k: k[0], reverse=True)
+        for id, puppet_properties in self.data:
+            print puppet_properties.get('puppet_modules', None)
+            print puppet_properties.get('puppet_manifest', None)
 
     def load_yaml(self, yaml_workbook):
         self.workbook = yaml.load(yaml_workbook)
